791ed9bb87218ddd65c79f9f4580789395c2a26f
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-12-02 17:00:11 +0700

    menambahkan default warna pada tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlack: '#0F0F0F',
+        primaryRed: '#E50915',
+        primaryGrey: '#E50915',
+        primaryYellow: '#FFD600',
+        secondaryGrey: '#282828',
+      },
+    },
   },
   plugins: [],
 };
